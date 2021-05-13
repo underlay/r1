893f82bb60a75730b5af05061c891571912f69b1
Samuel Klein <meta.sj@gmail.com>
2021-05-13 15:36:13 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
 const HomePage = () => {
-	return <div>Landing page</div>;
+	return (<div>
+			<h3>R<sub>0</sub>: an Underlay Registry</h3>
+			<p>Welcome to R<sub>0</sub>, a demonstration registry of data schemas, collections, and pipelines. </p>
+
+			<p><a href="https://r0.underlay.org/login">Log in</a> to experiment | 
+				Schema <a href="https://r0.underlay.org/docs">docs</a> & <a href="https://r0.underlay.org/login">examples</a>
+			</p> 			
+		</div>);
 };
 
 export default HomePage;
